8d206c9bc3ec96518a644a8c35e8d318dd1e13a6
Gabirel77 <gabriel.sm@estudante.iftm.edu.br>
2026-02-18 14:54:16 -0300

    WarmUp D solution of ICPC24 in Python

---
+++ DsolutionICPC24WarmUp.py
@@ -0,0 +1,30 @@
+def revigorante():
+    D_inicial = int(input())
+    C_atividades = int(input())
+    R_atividades = int(input())
+    total_atividades=R_atividades
+
+    listaC = []
+    listaR = []
+    for i in range(C_atividades):
+        listaC.append(int(input()))
+    for i in range(R_atividades):
+        listaR.append(int(input()))
+        D_inicial+=listaR[i]
+    menor = 1000000;
+    nova_lista=[]
+    for i in range(len(listaC)):
+        for j in range (len(listaC)):
+            if menor > listaC[j]:
+                menor=listaC[j]
+        nova_lista.append(menor)
+    
+    for k in range(len(listaC)):
+        if D_inicial - listaC[k] > 0:
+            D_inicial -= listaC[k]
+            total_atividades+=1
+        else:
+            break
+    print(total_atividades)
+
+revigorante()
