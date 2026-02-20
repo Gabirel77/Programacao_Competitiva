000941f2f1b9d2e8674ab9eea55e551af9135333
Gabirel77 <gabriel.sm@estudante.iftm.edu.br>
2026-02-20 17:44:36 -0300

    E solution of ICPC24 in Python without numpy

--- EsolutionICPC24.py
+++ EsolutionICPC24.py
@@ -1,24 +1,20 @@
-import numpy as np
-
 def estojo():
-    valori=0
-    valorj=0
-    menor=1000000
     N = int (input())
-    matriz = np.zeros((N,N))
+    menor =100000
+    indice = 0
+    cont = 0
     for i in range(N):
         lista = list(map(int,input().split()))
         for j in range(N):
-            matriz[i][j]= lista[j]
-            if matriz[i][j]< menor:
-                menor=matriz[i][j]
-                valori=i
-                valorj=j
-    if valori==valorj and valorj==0:
+            if menor>lista[j]:
+                menor = lista[j]
+                indice = j
+                cont=i
+    if indice==cont and cont==0:
         print(0)
-    elif valorj<valori:
+    elif indice<cont:
         print(3)
-    elif valorj>valori:
+    elif indice>cont:
         print(1)
     else:
         print(2)
